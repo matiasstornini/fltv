2c9429fc7a0f4b643bad389b0380fdd385342f91
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-01-26 23:29:30 -0300

    Add files via upload

---
+++ vue/est.js
@@ -0,0 +1,8 @@
+
+EstEspA = [];
+	var i3 = -1;
+for (var i = 0; i <= 30; i++) {
+	let EstEsp = ["<md-button class='md-raised md-primary' href='"+Est[i3=i3+1]+"' >Estadisticas</md-button> "];
+	EstEspA.push(EstEsp);
+}
+	console.log(EstEspA);
