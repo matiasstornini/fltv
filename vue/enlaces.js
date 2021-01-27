160c82d3ac5ab93661e1761ec5f45497218bbb3f
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-01-27 18:28:58 -0300

    Update enlaces.js

--- vue/enlaces.js
+++ vue/enlaces.js
@@ -1,7 +1,7 @@
 let AEsp = [
-"<md-button class='md-raised md-primary' href=' "+esp[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+esp[1]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+esp[2]+" ' >ABRIR 3</md-button><!-- <md-button class='md-raised md-primary'href='  href=' "+esp[3]+" ' >ABRIR 4</md-button>-->",
-"<md-button class='md-raised md-primary'href=' "+esp[3]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+esp[4]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+esp[5]+" ' >ABRIR 3</md-button>",
-"<md-button class='md-raised md-primary'href=' "+esp[6]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+esp[7]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+esp[8]+" ' >ABRIR 3</md-button>",
+"<md-button class='md-raised md-primary' href=' "+esp[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+esp[1]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+esp[2]+" ' >ABRIR 3</md-button><!-- <md-button class='md-raised md-primary'href=' "+esp[3]+" ' >ABRIR 4</md-button>-->",
+"<md-button class='md-raised md-primary'href=' "+esp[3]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+esp[4]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+esp[5]+" ' >ABRIR 3</md-button>",
+"<md-button class='md-raised md-primary'href=' "+esp[6]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+esp[7]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+esp[8]+" ' >ABRIR 3</md-button>",
 
 "<md-button class='md-raised md-primary'href=' "+esp[0]+" ' >ABRIR 1</md-button> ",
 "<md-button class='md-raised md-primary'href=' "+esp[1]+" ' >ABRIR 1</md-button> ",
@@ -18,9 +18,9 @@ let AEsp = [
 ];
 
 let AIng = [
-"<md-button class='md-raised md-primary'href=' "+ing[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+ing[1]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href='  href=' "+ing[2]+" ' >ABRIR 3</md-button>-->",
-"<md-button class='md-raised md-primary'href=' "+ing[2]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+ing[3]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href='  href=' "+ing[5]+" ' >ABRIR 3</md-button>-->",
-"<md-button class='md-raised md-primary'href=' "+ing[4]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+ing[5]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href='  href=' "+ing[8]+" ' >ABRIR 3</md-button>-->",
+"<md-button class='md-raised md-primary'href=' "+ing[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+ing[1]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href=' "+ing[2]+" ' >ABRIR 3</md-button>-->",
+"<md-button class='md-raised md-primary'href=' "+ing[2]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+ing[3]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href=' "+ing[5]+" ' >ABRIR 3</md-button>-->",
+"<md-button class='md-raised md-primary'href=' "+ing[4]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+ing[5]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href=' "+ing[8]+" ' >ABRIR 3</md-button>-->",
 
 "<md-button class='md-raised md-primary'href=' "+ing[0]+" ' >ABRIR 1</md-button> ",
 "<md-button class='md-raised md-primary'href=' "+ing[1]+" ' >ABRIR 1</md-button> ",
@@ -29,9 +29,9 @@ let AIng = [
 ];
 
 let AIta = [
-"<md-button class='md-raised md-primary'href=' "+ita[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+ita[1]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href='  href=' "+ita[2]+" ' >ABRIR 3</md-button>-->",
-"<md-button class='md-raised md-primary'href=' "+ita[2]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+ita[3]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href='  href=' "+ita[5]+" ' >ABRIR 3</md-button>-->",
-"<md-button class='md-raised md-primary'href=' "+ita[6]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+ita[7]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+ita[8]+" ' >ABRIR 3</md-button>",
+"<md-button class='md-raised md-primary'href=' "+ita[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+ita[1]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href=' "+ita[2]+" ' >ABRIR 3</md-button>-->",
+"<md-button class='md-raised md-primary'href=' "+ita[2]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+ita[3]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href=' "+ita[5]+" ' >ABRIR 3</md-button>-->",
+"<md-button class='md-raised md-primary'href=' "+ita[6]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+ita[7]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+ita[8]+" ' >ABRIR 3</md-button>",
 "<md-button class='md-raised md-primary'href=' "+ita[0]+" ' >ABRIR 1</md-button> ",
 "<md-button class='md-raised md-primary'href=' "+ita[1]+" ' >ABRIR 1</md-button> ",
 "<md-button class='md-raised md-primary'href=' "+ita[2]+" ' >ABRIR 1</md-button> ",
@@ -41,50 +41,50 @@ let AIta = [
 ];
 
 let AAle = [
-"<md-button class='md-raised md-primary'href=' "+ale[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+ale[1]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+ale[2]+" ' >ABRIR 3</md-button>",
-"<md-button class='md-raised md-primary'href=' "+ale[2]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+ale[3]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href='  href=' "+ale[5]+" ' >ABRIR 3</md-button>-->",
-"<md-button class='md-raised md-primary'href=' "+ale[6]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+ale[7]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+ale[8]+" ' >ABRIR 3</md-button>"
+"<md-button class='md-raised md-primary'href=' "+ale[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+ale[1]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+ale[2]+" ' >ABRIR 3</md-button>",
+"<md-button class='md-raised md-primary'href=' "+ale[2]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+ale[3]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href=' "+ale[5]+" ' >ABRIR 3</md-button>-->",
+"<md-button class='md-raised md-primary'href=' "+ale[6]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+ale[7]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+ale[8]+" ' >ABRIR 3</md-button>"
 
 ];
 let AFra = [
-"<md-button class='md-raised md-primary'href=' "+fra[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+fra[1]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href='  href=' "+fra[2]+" ' >ABRIR 3</md-button>-->",
-"<md-button class='md-raised md-primary'href=' "+fra[2]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+fra[3]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href='  href=' "+fra[5]+" ' >ABRIR 3</md-button>-->",
-"<md-button class='md-raised md-primary'href=' "+fra[6]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+fra[7]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+fra[8]+" ' >ABRIR 3</md-button>"
+"<md-button class='md-raised md-primary'href=' "+fra[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+fra[1]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href=' "+fra[2]+" ' >ABRIR 3</md-button>-->",
+"<md-button class='md-raised md-primary'href=' "+fra[2]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+fra[3]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href=' "+fra[5]+" ' >ABRIR 3</md-button>-->",
+"<md-button class='md-raised md-primary'href=' "+fra[6]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+fra[7]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+fra[8]+" ' >ABRIR 3</md-button>"
 
 ];
 let AUcl = [
-"<md-button class='md-raised md-primary'href=' "+champions[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+champions[1]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+champions[2]+" ' >ABRIR 3</md-button>",
-"<md-button class='md-raised md-primary'href=' "+champions[3]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+champions[4]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+champions[5]+" ' >ABRIR 3</md-button>",
-"<md-button class='md-raised md-primary'href=' "+champions[6]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+champions[7]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+champions[8]+" ' >ABRIR 3</md-button>",
-"<md-button class='md-raised md-primary'href=' "+champions[9]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+champions[10]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+champions[11]+" ' >ABRIR 3</md-button>"
+"<md-button class='md-raised md-primary'href=' "+champions[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+champions[1]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+champions[2]+" ' >ABRIR 3</md-button>",
+"<md-button class='md-raised md-primary'href=' "+champions[3]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+champions[4]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+champions[5]+" ' >ABRIR 3</md-button>",
+"<md-button class='md-raised md-primary'href=' "+champions[6]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+champions[7]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+champions[8]+" ' >ABRIR 3</md-button>",
+"<md-button class='md-raised md-primary'href=' "+champions[9]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+champions[10]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+champions[11]+" ' >ABRIR 3</md-button>"
 
 ];
 let AEuro = [
-"<md-button class='md-raised md-primary'href=' "+euro[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+euro[1]+" ' >ABRIR 2</md-button>",
+"<md-button class='md-raised md-primary'href=' "+euro[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+euro[1]+" ' >ABRIR 2</md-button>",
 "<md-button class='md-raised md-primary'href=' "+euro[0]+" ' >ABRIR 1</md-button>",
 "<md-button class='md-raised md-primary'href=' "+euro[1]+" ' >ABRIR 2</md-button>"
 ];
 let AConmebol = [
-"<md-button class='md-raised md-primary'href=' "+conmebol[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+conmebol[1]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href='  href=' "+conmebol[2]+" ' >ABRIR 3</md-button>-->",
+"<md-button class='md-raised md-primary'href=' "+conmebol[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+conmebol[1]+" ' >ABRIR 2</md-button> <!--<md-button class='md-raised md-primary'href=' "+conmebol[2]+" ' >ABRIR 3</md-button>-->",
 
-"<md-button class='md-raised md-primary'href=' "+conmebol[2]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+conmebol[3]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+conmebol[4]+" ' >ABRIR 3</md-button>",
+"<md-button class='md-raised md-primary'href=' "+conmebol[2]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+conmebol[3]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+conmebol[4]+" ' >ABRIR 3</md-button>",
 
-"<md-button class='md-raised md-primary'href=' "+conmebol[5]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+conmebol[6]+" ' >ABRIR 2</md-button> ",
+"<md-button class='md-raised md-primary'href=' "+conmebol[5]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+conmebol[6]+" ' >ABRIR 2</md-button> ",
 
-"<md-button class='md-raised md-primary'href=' "+conmebol[9]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+conmebol[10]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+conmebol[11]+" ' >ABRIR 3</md-button>",
+"<md-button class='md-raised md-primary'href=' "+conmebol[9]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+conmebol[10]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+conmebol[11]+" ' >ABRIR 3</md-button>",
 
 ];
 let ALib = [
-"<md-button class='md-raised md-primary'href=' "+lib[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+lib[1]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+lib[2]+" ' >ABRIR 3</md-button>",
-"<md-button class='md-raised md-primary'href=' "+lib[3]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+lib[4]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+lib[5]+" ' >ABRIR 3</md-button>",
-"<md-button class='md-raised md-primary'href=' "+lib[6]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+lib[7]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+lib[8]+" ' >ABRIR 3</md-button>",
-"<md-button class='md-raised md-primary'href=' "+lib[9]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+lib[10]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+lib[11]+" ' >ABRIR 3</md-button>",
+"<md-button class='md-raised md-primary'href=' "+lib[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+lib[1]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+lib[2]+" ' >ABRIR 3</md-button>",
+"<md-button class='md-raised md-primary'href=' "+lib[3]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+lib[4]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+lib[5]+" ' >ABRIR 3</md-button>",
+"<md-button class='md-raised md-primary'href=' "+lib[6]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+lib[7]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+lib[8]+" ' >ABRIR 3</md-button>",
+"<md-button class='md-raised md-primary'href=' "+lib[9]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+lib[10]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+lib[11]+" ' >ABRIR 3</md-button>",
 
 ];
 let AArg = [
-"<md-button class='md-raised md-primary'href=' "+arg[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+arg[1]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+arg[2]+" ' >ABRIR 3</md-button>",
-"<md-button class='md-raised md-primary'href=' "+arg[3]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+arg[4]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+arg[5]+" ' >ABRIR 3</md-button>",
-"<md-button class='md-raised md-primary'href=' "+arg[6]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+arg[7]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href='  href=' "+esp[8]+" ' >ABRIR 3</md-button>",
+"<md-button class='md-raised md-primary'href=' "+arg[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+arg[1]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+arg[2]+" ' >ABRIR 3</md-button>",
+"<md-button class='md-raised md-primary'href=' "+arg[3]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+arg[4]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+arg[5]+" ' >ABRIR 3</md-button>",
+"<md-button class='md-raised md-primary'href=' "+arg[6]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+arg[7]+" ' >ABRIR 2</md-button> <md-button class='md-raised md-primary'href=' "+esp[8]+" ' >ABRIR 3</md-button>",
 
 "<md-button class='md-raised md-primary'href=' "+arg[0]+" ' >ABRIR 1</md-button> ",
 "<md-button class='md-raised md-primary'href=' "+arg[1]+" ' >ABRIR 1</md-button> ",
@@ -107,7 +107,7 @@ let ACol = [
 ];
 
 let AMex = [
-"<md-button class='md-raised md-primary'href=' "+mex[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href='  href=' "+mex[1]+" ' >ABRIR 2</md-button>",
+"<md-button class='md-raised md-primary'href=' "+mex[0]+" ' >ABRIR 1</md-button> <md-button class='md-raised md-primary'href=' "+mex[1]+" ' >ABRIR 2</md-button>",
 
 "<md-button class='md-raised md-primary'href=' "+mex[0]+" ' >ABRIR 1</md-button>",
 "<md-button class='md-raised md-primary'href=' "+mex[1]+" ' >ABRIR 2</md-button>",
