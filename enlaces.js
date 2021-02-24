7dd9416353ce89f6ec89a5853351dc9bbc651ed6
adiccfut <37271131+adiccfut@users.noreply.github.com>
2021-02-24 17:03:31 -0300

    Add files via upload

--- enlaces.js
+++ enlaces.js
@@ -2,39 +2,22 @@ let AEsp = [
 "<a class='btn btn-primary' href=' "+esp[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+esp[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+esp[2]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a><!-- <a class='btn btn-primary' href=' "+esp[3]+" ' style='background-color: #ffffff00;'><b>ABRIR 4</b></a>-->",
 "<a class='btn btn-primary' href=' "+esp[3]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+esp[4]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+esp[5]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
 "<a class='btn btn-primary' href=' "+esp[6]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+esp[7]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+esp[8]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
-
-"<a class='btn btn-primary' href=' "+esp[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+esp[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+esp[2]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+esp[3]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+esp[4]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+esp[5]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+esp[6]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+esp[7]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+esp[8]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> "
+"<a class='btn btn-primary' href=' "+esp[9]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> "
 
 ];
 
 let AIng = [
 "<a class='btn btn-primary' href=' "+ing[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ing[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+ing[2]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>-->",
 "<a class='btn btn-primary' href=' "+ing[2]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ing[3]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+ing[5]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>-->",
-"<a class='btn btn-primary' href=' "+ing[4]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ing[5]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+ing[8]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>-->",
-
-"<a class='btn btn-primary' href=' "+ing[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ","<a class='btn btn-primary' href=' "+ing[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ","<a class='btn btn-primary' href=' "+ing[2]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> "
+"<a class='btn btn-primary' href=' "+ing[4]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ing[5]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+ing[8]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>-->"
 
 ];
 
 let AIta = [
 "<a class='btn btn-primary' href=' "+ita[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ita[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+ita[2]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>-->",
 "<a class='btn btn-primary' href=' "+ita[2]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ita[3]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+ita[5]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>-->",
-"<a class='btn btn-primary' href=' "+ita[6]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ita[7]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+ita[8]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
+"<a class='btn btn-primary' href=' "+ita[6]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ita[7]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+ita[8]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>"
 
-"<a class='btn btn-primary' href=' "+ita[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+ita[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+ita[2]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+ita[3]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+ita[4]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+ita[5]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> "
 ];
 
 let AAle = [
@@ -49,44 +32,25 @@ let AFra = [
 "<a class='btn btn-primary' href=' "+fra[6]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+fra[7]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+fra[8]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>"
 
 ];
-let AUcl = [
-"<a class='btn btn-primary' href=' "+champions[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+champions[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+champions[2]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
-"<a class='btn btn-primary' href=' "+champions[3]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+champions[4]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+champions[5]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
-"<a class='btn btn-primary' href=' "+champions[6]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+champions[7]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+champions[8]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
-"<a class='btn btn-primary' href=' "+champions[9]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+champions[10]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+champions[11]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>"
 
-];
 let AEuro = [
 "<a class='btn btn-primary' href=' "+euro[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+euro[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a>"
 ];
 let AConmebol = [
 "<a class='btn btn-primary' href=' "+conmebol[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+conmebol[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+conmebol[2]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>-->",
-  
-"<a class='btn btn-primary' href=' "+conmebol[2]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+conmebol[3]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+conmebol[4]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
-  
-"<a class='btn btn-primary' href=' "+conmebol[5]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+conmebol[6]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a>",
-  
+"<a class='btn btn-primary' href=' "+conmebol[2]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+conmebol[3]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+conmebol[3]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
+"<a class='btn btn-primary' href=' "+conmebol[6]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+conmebol[7]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+conmebol[8]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
 "<a class='btn btn-primary' href=' "+conmebol[9]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+conmebol[10]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+conmebol[11]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
 
 ];
 let ALib = [
 "<a class='btn btn-primary' href=' "+lib[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+lib[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+lib[2]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
+
 "<a class='btn btn-primary' href=' "+lib[3]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+lib[4]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+lib[5]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
+
 "<a class='btn btn-primary' href=' "+lib[6]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+lib[7]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+lib[8]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
+
 "<a class='btn btn-primary' href=' "+lib[9]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+lib[10]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+lib[11]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
-
-];
-let AArg = [
-"<a class='btn btn-primary' href=' "+arg[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+arg[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+arg[2]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
-"<a class='btn btn-primary' href=' "+arg[3]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+arg[4]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+arg[5]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
-"<a class='btn btn-primary' href=' "+arg[6]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+arg[7]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+esp[8]+" ' style='background-color: #ffffff00;'><b>ABRIR 3</b></a>",
-
-"<a class='btn btn-primary' href=' "+arg[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+arg[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+arg[2]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+arg[3]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+arg[4]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> ",
-"<a class='btn btn-primary' href=' "+arg[5]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> "
 
 ];
 
@@ -96,14 +60,10 @@ let ACol = [
 ];
 
 let AMex = [
-"<a class='btn btn-primary' href=' "+mex[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+mex[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a>",
-
-"<a class='btn btn-primary' href=' "+mex[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a>",
-"<a class='btn btn-primary' href=' "+mex[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a>",
-"<a class='btn btn-primary' href=' "+mex[2]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a>"
+"<a class='btn btn-primary' href=' "+mex[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+mex[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a>"
 ];
 
-let AGo = [
-"<a class='btn btn-primary' href=' "+go[0]+" ' style='background-color: #ffffff00;'><b>ABRIR (WebCast- Prueba)</b></a>",
-"<a class='btn btn-primary' href=' "+go[1]+" ' style='background-color: #ffffff00;'><b>Más</b></a>"  
+let CPA = [
+"<a class='btn btn-primary' href=' "+cpa[0]+" ' style='background-color: #ffffff00;'><b>ABRIR 1</b></a>",
+"<a class='btn btn-primary' href=' "+cpa[1]+" ' style='background-color: #ffffff00;'><b>ABRIR 2</b></a>"
 ];
