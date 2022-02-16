f4947508f8cbccab9da7f3270865dbcc19536d25
adiccfut <37271131+adiccfut@users.noreply.github.com>
2022-02-16 00:56:27 -0300

    Add files via upload

---
+++ iOS/enlaces.js
@@ -0,0 +1,145 @@
+let AEsp = [
+"<a class='btn btn-primary' href=' "+esp[0]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+esp[1]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+esp[2]+" ' ><b>ABRIR 3</b></a><!-- <a class='btn btn-primary' href=' "+esp[3]+" ' ><b>ABRIR 4</b></a>-->",
+"<a class='btn btn-primary' href=' "+esp[3]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+esp[4]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+esp[5]+" ' ><b>ABRIR 3</b></a>",
+"<a class='btn btn-primary' href=' "+esp[6]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+esp[7]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+esp[8]+" ' ><b>ABRIR 3</b></a>",
+
+"<a class='btn btn-primary' href=' "+esp[0]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+esp[1]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+esp[2]+" ' ><b>ABRIR 1</b></a> ",
+
+"<a class='btn btn-primary' href=' "+esp[3]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+esp[4]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+esp[5]+" ' ><b>ABRIR 1</b></a> ",
+
+"<a class='btn btn-primary' href=' "+esp[6]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+esp[7]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+esp[8]+" ' ><b>ABRIR 1</b></a> "
+
+];
+
+let AIng = [
+"<a class='btn btn-primary' href=' "+ing[0]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ing[1]+" ' ><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+ing[2]+" ' ><b>ABRIR 3</b></a>-->",
+"<a class='btn btn-primary' href=' "+ing[2]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ing[3]+" ' ><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+ing[5]+" ' ><b>ABRIR 3</b></a>-->",
+"<a class='btn btn-primary' href=' "+ing[4]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ing[5]+" ' ><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+ing[8]+" ' ><b>ABRIR 3</b></a>-->",
+
+"<a class='btn btn-primary' href=' "+ing[0]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+ing[1]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+ing[2]+" ' ><b>ABRIR 1</b></a> "
+
+];
+
+let AIta = [
+"<a class='btn btn-primary' href=' "+ita[0]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ita[1]+" ' ><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+ita[2]+" ' ><b>ABRIR 3</b></a>-->",
+"<a class='btn btn-primary' href=' "+ita[2]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ita[3]+" ' ><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+ita[5]+" ' ><b>ABRIR 3</b></a>-->",
+"<a class='btn btn-primary' href=' "+ita[6]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ita[7]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+ita[8]+" ' ><b>ABRIR 3</b></a>",
+"<a class='btn btn-primary' href=' "+ita[0]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+ita[1]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+ita[2]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+ita[3]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+ita[4]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+ita[5]+" ' ><b>ABRIR 1</b></a> "
+];
+
+let AAle = [
+"<a class='btn btn-primary' href=' "+ale[0]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ale[1]+" ' ><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+ale[2]+" ' ><b>ABRIR 3</b></a>-->",
+"<a class='btn btn-primary' href=' "+ale[2]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ale[3]+" ' ><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+ale[5]+" ' ><b>ABRIR 3</b></a>-->",
+"<a class='btn btn-primary' href=' "+ale[6]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+ale[7]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+ale[8]+" ' ><b>ABRIR 3</b></a>"
+
+];
+let AFra = [
+"<a class='btn btn-primary' href=' "+fra[0]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+fra[1]+" ' ><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+fra[2]+" ' ><b>ABRIR 3</b></a>-->",
+"<a class='btn btn-primary' href=' "+fra[2]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+fra[3]+" ' ><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+fra[5]+" ' ><b>ABRIR 3</b></a>-->",
+"<a class='btn btn-primary' href=' "+fra[6]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+fra[7]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+fra[8]+" ' ><b>ABRIR 3</b></a>"
+
+];
+let AUcl = [
+"<a class='btn btn-primary' href=' "+champions[0]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+champions[1]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+champions[2]+" ' ><b>ABRIR 3</b></a>",
+"<a class='btn btn-primary' href=' "+champions[3]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+champions[4]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+champions[5]+" ' ><b>ABRIR 3</b></a>",
+"<a class='btn btn-primary' href=' "+champions[6]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+champions[7]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+champions[8]+" ' ><b>ABRIR 3</b></a>",
+"<a class='btn btn-primary' href=' "+champions[9]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+champions[10]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+champions[11]+" ' ><b>ABRIR 3</b></a>"
+
+];
+let AEuro = [
+"<a class='btn btn-primary' href=' "+euro[0]+" ' ><b>ABRIR 1</b></a>",
+"<a class='btn btn-primary' href=' "+euro[1]+" ' ><b>ABRIR 2</b></a>",
+"<a class='btn btn-primary' href=' "+euro[0]+" ' ><b>ABRIR 1</b></a>",
+"<a class='btn btn-primary' href=' "+euro[1]+" ' ><b>ABRIR 2</b></a>"
+];
+let AConmebol = [
+"<a class='btn btn-primary' href=' "+conmebol[0]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+conmebol[1]+" ' ><b>ABRIR 2</b></a> <!--<a class='btn btn-primary' href=' "+conmebol[2]+" ' ><b>ABRIR 3</b></a>-->",
+
+"<a class='btn btn-primary' href=' "+conmebol[2]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+conmebol[3]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+conmebol[4]+" ' ><b>ABRIR 3</b></a>",
+
+"<a class='btn btn-primary' href=' "+conmebol[5]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+conmebol[6]+" ' ><b>ABRIR 2</b></a> ",
+
+"<a class='btn btn-primary' href=' "+conmebol[9]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+conmebol[10]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+conmebol[11]+" ' ><b>ABRIR 3</b></a>",
+
+];
+let ALib = [
+"<a class='btn btn-primary' href=' "+lib[0]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+lib[1]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+lib[2]+" ' ><b>ABRIR 3</b></a>",
+"<a class='btn btn-primary' href=' "+lib[3]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+lib[4]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+lib[5]+" ' ><b>ABRIR 3</b></a>",
+"<a class='btn btn-primary' href=' "+lib[6]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+lib[7]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+lib[8]+" ' ><b>ABRIR 3</b></a>",
+"<a class='btn btn-primary' href=' "+lib[9]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+lib[10]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+lib[11]+" ' ><b>ABRIR 3</b></a>",
+
+"<a class='btn btn-primary' href=' "+lib[0]+" ' ><b>ABRIR 1</b></a>",
+"<a class='btn btn-primary' href=' "+lib[1]+" ' ><b>ABRIR 2</b></a>",
+"<a class='btn btn-primary' href=' "+lib[2]+" ' ><b>ABRIR 3</b></a>",
+];
+let AArg = [
+"<a class='btn btn-primary' href=' "+arg[0]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+arg[1]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+arg[2]+" ' ><b>ABRIR 3</b></a>",
+"<a class='btn btn-primary' href=' "+arg[3]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+arg[4]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+arg[5]+" ' ><b>ABRIR 3</b></a>",
+"<a class='btn btn-primary' href=' "+arg[6]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+arg[7]+" ' ><b>ABRIR 2</b></a> <a class='btn btn-primary' href=' "+esp[8]+" ' ><b>ABRIR 3</b></a>",
+
+"<a class='btn btn-primary' href=' "+arg[0]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+arg[1]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+arg[2]+" ' ><b>ABRIR 1</b></a> ",
+
+"<a class='btn btn-primary' href=' "+arg[3]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+arg[4]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+arg[5]+" ' ><b>ABRIR 1</b></a> "
+
+];
+
+let ACol = [
+"<a class='btn btn-primary' href=' "+col[0]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+col[1]+" ' ><b>ABRIR 2</b></a>",
+
+"<a class='btn btn-primary' href=' "+col[2]+" ' ><b>ABRIR 3</b></a>",
+
+"<a class='btn btn-primary' href=' "+col[0]+" ' ><b>ABRIR 1</b></a>",
+"<a class='btn btn-primary' href=' "+col[1]+" ' ><b>ABRIR 2</b></a>",
+"<a class='btn btn-primary' href=' "+col[2]+" ' ><b>ABRIR 3</b></a>"
+];
+
+let AMex = [
+"<a class='btn btn-primary' href=' "+mex[0]+" ' ><b>ABRIR 1</b></a> <a class='btn btn-primary' href=' "+mex[1]+" ' ><b>ABRIR 2</b></a>",
+
+"<a class='btn btn-primary' href=' "+mex[0]+" ' ><b>ABRIR 1</b></a>",
+"<a class='btn btn-primary' href=' "+mex[1]+" ' ><b>ABRIR 2</b></a>",
+"<a class='btn btn-primary' href=' "+mex[2]+" ' ><b>ABRIR 1</b></a>"
+];
+
+let AGo = [
+"<a class='btn btn-primary' href=' "+go[0]+" ' ><b>ABRIR (WebCast- Prueba)</b></a>",
+"<a class='btn btn-primary' href=' "+go[1]+" ' ><b>ABRIR (WebCast- Prueba)</b></a>",
+"<a class='btn btn-primary' href=' "+go[2]+" ' ><b>ABRIR (WebCast- Prueba)</b></a>",
+"<a class='btn btn-primary' href=' "+go[3]+" ' ><b>ABRIR (WebCast- Prueba)</b></a>",
+"<a class='btn btn-primary' href=' "+go[4]+" ' ><b>Más</b></a>",
+
+];
+
+let CPA = [
+"<a class='btn btn-primary' href=' "+cpa[0]+" '  target='_blank'><b>ABRIR FHD</b></a>",
+
+"<a class='btn btn-primary' href=' "+go[1]+" ' ><b>Más</b></a>"
+];
+
+let Extra = [
+"<a class='btn btn-primary' href=' "+ext[0]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+ext[1]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+ext[2]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+ext[3]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+ext[4]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+ext[5]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+ext[6]+" ' ><b>ABRIR 1</b></a> ",
+"<a class='btn btn-primary' href=' "+ext[7]+" ' ><b>ABRIR 1</b></a> "
+]
---
+++ iOS/est.js
@@ -0,0 +1,8 @@
+
+EstEspA = [];
+	var i3 = -1;
+for (var i = 0; i <= 30; i++) {
+	let EstEsp = ["<a class='btn btn-primary'  href='"+Est[i3=i3+1]+"' >Estadisticas</a> "];
+	EstEspA.push(EstEsp);
+}
+	console.log(EstEspA);
